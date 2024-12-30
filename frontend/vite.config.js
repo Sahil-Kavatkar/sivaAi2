dab258201aa2a2ba9f8e7ed01558e154fc86dc66
Sahil Kavatkar <sahilkavatkar@gmail.com>
2024-12-30 21:33:22 +0530

    project5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,4 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Ensure the build output goes to the correct folder
   },
-  server: {
-    port: 5173, // Only required if you are working locally and need to specify a port
-    host: true, // Expose server to the network (useful in local dev to access from other devices)
-  },
 });
